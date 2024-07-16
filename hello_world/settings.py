7f8066ea3bb8a5a3b132dda1fcd9f92363ee123f
Praisesomiji <67150379+Praisesomiji@users.noreply.github.com>
2024-07-16 00:27:48 +0000

    Install newly created apps

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -44,6 +44,10 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "django_browser_reload",
+    "users",
+    "courses",
+    "grades",
+    "notifications",
 ]
 
 MIDDLEWARE = [
