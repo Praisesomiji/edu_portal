c050c0da11dfd3e0492067af7e30af74ce270900
Praisesomiji <67150379+Praisesomiji@users.noreply.github.com>
2024-07-16 08:55:25 +0000

    Create test cases for views

--- users/tests.py
+++ users/tests.py
@@ -25,3 +25,24 @@ class UserModelTests(TestCase):
     def test_adviser_profile_creation(self):
         adviser_profile = AdviserProfile.objects.get(user=self.user_adviser)
         self.assertEqual(adviser_profile.department, 'Computer Science')
+
+from django.urls import reverse
+from django.test import Client
+
+class UserViewsTests(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.user_student = get_user_model().objects.create_user(username='student_user', password='testpass123', role='student')
+        self.user_adviser = get_user_model().objects.create_user(username='adviser_user', password='testpass123', role='adviser')
+
+    def test_student_dashboard_view(self):
+        self.client.login(username='student_user', password='testpass123')
+        response = self.client.get(reverse('student_dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/student_dashboard.html')
+
+    def test_adviser_dashboard_view(self):
+        self.client.login(username='adviser_user', password='testpass123')
+        response = self.client.get(reverse('adviser_dashboard'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'users/adviser_dashboard.html')
