c4e38cc9ab91aedf4cfedac6aab68104eda3628c
Praisesomiji <67150379+Praisesomiji@users.noreply.github.com>
2024-07-16 14:01:20 +0000

    Set suth user model

--- hello_world/settings.py
+++ hello_world/settings.py
@@ -147,3 +147,5 @@ MEDIA_ROOT = BASE_DIR / "hello_world" / "media"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = 'users.User'
--- users/tests.py
+++ users/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from django.contrib.auth import get_user_model
+# from django.contrib.auth import get_user_model
 from .models import StudentProfile, AdviserProfile, User
 
 class UserModelTests(TestCase):
